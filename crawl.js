a3631f844543d04f53c861e8155cc673279c8f22
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-03-29 15:50:16 +0300

    Added a crawlPage recursive function

--- crawl.js
+++ crawl.js
@@ -1,24 +1,49 @@
 const { JSDOM } = require('jsdom')
+const axios = require('axios');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizeCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizeCurrentURL] > 0){
+        pages[normalizeCurrentURL]++
+        return pages
+    }
+    pages[normalizeCurrentURL] = 1 
+
+    console.log(`actively crawling at ${currentURL}`)
+
    try {
-    const res = await fetch(currentURL)
+    const res = await axios.get(currentURL)
 
     if (res.status > 399){
         console.log(`error in fetch with status code ${res.status}, on page: ${currentURL}`)
-        return
+        return pages
     }
 
-    const contentType = res.header.get("content-type")
+    const contentType = res.headers['content-type']
     if(!contentType.includes("text/html")){
         console.log(`non HTML page, content-type: ${contentType}, on page: ${currentURL}`)
-        return
+        return pages
     }
 
-    console.log(await res.text())
+    const htmlBody = await res.data
+
+    const nextURLS = getURLsFromHTML(htmlBody, baseURL)
+
+    for (const nextURL of nextURLS){
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
    } catch (error) {
     console.log(`error in fetch: ${error.message}, on page: ${currentURL}`)
    }
+   return pages
 }
 
 
@@ -33,14 +58,14 @@ function getURLsFromHTML( htmlBody, baseURL){
                 const urlObj = new URL (`${baseURL}${linkELement.href}`)
                 urls.push (urlObj.href)
             } catch (error) {
-                console.log(`error with relative url${error.message}`)
+                console.log(`error with relative url: ${error.message}`)
             }
         }else{
             try {
                 const urlObj = new URL (`${linkELement.href}`)
                 urls.push (urlObj.href)
             } catch (error) {
-                console.log(`error with absolute url${error.message}`)
+                console.log(`error with absolute url: ${error.message}`)
             }
         }
     }
